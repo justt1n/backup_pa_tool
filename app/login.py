534c330dba5f9cef1cda2fc9f95bc66c23faf8ec
justt1n <tinnt0709@gmail.com>
2025-02-15 13:46:01 +0700

    update

--- app/login.py
+++ app/login.py
@@ -48,7 +48,7 @@ def login(
     ### upload currency file
     for file in list_files_in_output('storage/output/currency'):
         sendCurrencyFile(_browser, file)
-        print(f"Upload {file}")
+        print(f"Uploaded")
         time.sleep(_time_sleep)
         print(f"Sleeping for {_time_sleep} seconds")
 
@@ -56,7 +56,7 @@ def login(
         ### upload item file
         for file in list_files_in_output('storage/output/item'):
             sendItemFile(_browser, file)
-            print(f"Upload {file}")
+            print(f"Uploaded")
             time.sleep(_time_sleep)
             print(f"Sleeping for {_time_sleep} seconds")
 
@@ -82,7 +82,6 @@ def sendCurrencyFile(_browser: SeleniumUtil, path: str) -> None:
     browse_button.click()
     _browser.click_by_inner_text("UPLOAD")
     time.sleep(15)
-    _browser.close()
 
 
 def sendItemFile(_browser: SeleniumUtil, path: str) -> None:
@@ -103,7 +102,6 @@ def sendItemFile(_browser: SeleniumUtil, path: str) -> None:
     browse_button.click()
     _browser.click_by_inner_text("UPLOAD")
     time.sleep(15)
-    _browser.close()
 
 
 if __name__ == "__main__":
